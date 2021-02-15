bcf46924067cd38b518ce6d60d3400c076adcfb2
Caleb Rees Tulloss <caleb.tulloss@columbia.edu>
2021-02-15 14:39:37 -0500

    manually copy all file structure from new esp version

--- rtl/include/grlib/stdlib/version.vhd
+++
@@ -1,5 +0,0 @@
-
-package version is
-  constant grlib_version : integer := 1500;
-  constant grlib_build : integer := 4164;
-end;
--- socs/common/ariane_soc_pkg.sv
+++
@@ -1,8 +0,0 @@
-// ${ARIANE}/include/ariane_axi_pkg.sv requires these params defined here
-
-package ariane_soc;
-
-    localparam IdWidth = 4;
-    localparam IdWidthSlave = IdWidth;
-
-endpackage
